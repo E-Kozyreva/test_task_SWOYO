fce7433fe3e06bd554af48656b68b368675635e1
E-Kozyreva <kozyreva.k1@mail.ru>
2023-10-19 14:11:50 +0300

    add prime and roman_to_int algorithms

---
+++ algorithms/prime_numbers.py
@@ -0,0 +1,24 @@
+import time
+from math import ceil
+
+
+def SieveOfEratosthenes(n: int) -> list:
+    prime = [True for i in range(n + 1)] 
+    p = 2
+    
+    while(p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    
+    nums = [p for p in range(2, n) if prime[p]]
+    return nums
+
+
+def prime_numbers(low: int, high: int) -> list:
+    if (isinstance(low, (int, float)) and 
+        isinstance(high, (int, float)) and 
+        low <= high):
+        return [elem for elem in SieveOfEratosthenes(int(high)) if elem >= ceil(low)]
+    return []
---
+++ algorithms/roman_to_int.py
@@ -0,0 +1,27 @@
+import time
+from re import match
+
+
+def is_roman_number(num: str) -> bool:
+    reg = r'^M{0,3}(CM|CD|D?C{0,3})?(XC|XL|L?X{0,3})?(IX|IV|V?I{0,3})?$'
+    return True if match(reg, num) else False
+
+
+def roman_numerals_to_int(s: str) -> int:
+    if not is_roman_number(s):
+        return None
+    translations = {"I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000
+                    }
+    number = 0
+    s = s.replace("IV", "IIII").replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+    for char in s:
+        number += translations[char]
+    return number
