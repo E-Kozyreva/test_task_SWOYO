343cf4d7dc0050a17ab456c43386d8c58e660d71
E-Kozyreva <kozyreva.k1@mail.ru>
2023-10-22 15:35:44 +0300

    check pep8

--- algorithms/roman_to_int.py
+++ algorithms/roman_to_int.py
@@ -10,18 +10,20 @@ def is_roman_number(num: str) -> bool:
 def roman_numerals_to_int(s: str) -> int:
     if not is_roman_number(s):
         return None
+
     translations = {"I": 1,
                     "V": 5,
                     "X": 10,
                     "L": 50,
                     "C": 100,
                     "D": 500,
-                    "M": 1000
-                    }
+                    "M": 1000}
     number = 0
     s = s.replace("IV", "IIII").replace("IX", "VIIII")
     s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
     s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
     for char in s:
         number += translations[char]
+
     return number
---
+++ algorithms/text_statistics.py
@@ -0,0 +1,75 @@
+import re
+
+
+class TextStatistics:
+    def __init__(self):
+        self.text = ""
+        self.words = []
+        self.letters = ()
+        self.frequency = {}
+        self.bilingual_words = []
+
+    # текст из файла
+    def get_text(self, file_path: str) -> str:
+        with open(file_path, 'r', encoding='UTF-8') as file:
+            self.text = file.read()
+
+    # слова из текста
+    def get_words(self) -> list:
+        self.words = re.findall(r'\w+', self.text)
+        return self.words
+
+    # количество слов
+    def words_amount(self) -> int:
+        return len(self.words)
+
+    # буквы из слов
+    def get_letters(self) -> tuple:
+        self.letters = tuple("".join(self.words))
+        return self.letters
+
+    # количество букв и их частоту
+    def letters_frequency(self) -> dict:
+        split_text = "".join(self.words)
+
+        for letter in self.letters:
+            f_letter = split_text.count(letter)
+            p_letter = f_letter / len(self.words)
+            self.frequency[letter] = (f_letter, p_letter)
+
+        return self.frequency
+
+    # количество параграфов
+    def paragraph_amount(self) -> int:
+        return self.text.count("\n\n")
+
+    # слова с включающие буквы из разных алфавитов
+    def bilingual_word_amount(self) -> int:
+        for w in self.words:
+            if re.search(r'[a-zA-Z]', w) and re.search(r'[а-яА-Я]', w):
+                self.bilingual_words.append(w)   
+
+        return self.bilingual_words
+
+
+def text_statistics(file_path: str) -> tuple:
+    ts = TextStatistics()
+    ts.get_text(file_path)
+    ts.get_words()
+    ts.get_letters()
+
+    w_amount = ts.words_amount()
+    l_frequency = ts.letters_frequency()
+    p_amount = ts.paragraph_amount()
+    b_words = ts.bilingual_word_amount()
+
+    text_statistics = {}
+
+    for key, value in l_frequency.items():
+        text_statistics[key] = value
+
+    text_statistics["word_amount"] = w_amount
+    text_statistics["paragraph_amount"] = p_amount
+    text_statistics["bilingual_word_amount"] = len(b_words)
+
+    return text_statistics
